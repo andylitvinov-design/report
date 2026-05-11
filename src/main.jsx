cb23ecfd18c0ef88c73802baefa7a888c9cb20a6
andylitvinov-design <andy.litvinov@gmail.com>
2026-05-11 06:51:40 -0400

    Add alchemy v1 preview routes

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/report-preview/2" replace />} />
+        <Route path="/" element={<Navigate to="/report-preview/alchemy-v1/all" replace />} />
         <Route path="/report-preview" element={<Navigate to="/report-preview/2" replace />} />
         <Route path="/report-preview/all" element={<App mode="all" />} />
+        <Route path="/report-preview/alchemy-v1" element={<Navigate to="/report-preview/alchemy-v1/all" replace />} />
+        <Route path="/report-preview/alchemy-v1/all" element={<App mode="all" template="alchemy" />} />
+        <Route path="/report-preview/alchemy-v1/:pageId" element={<App mode="single" template="alchemy" />} />
         <Route path="/report-preview/:pageId" element={<App mode="single" />} />
       </Routes>
     </HashRouter>
